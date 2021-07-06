cecffb5d315060fd33eba51128d72995a1751ebb
Jhony de Oliveira Rodrigues <jhony.o_rodrigues@hotmail.com>
2021-07-06 12:03:04 -0300

    utilizando o tipo função

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,7 +1,37 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.teste.testaNullable
-
 fun main() {
-    testaNullable()
+//    testaTipoFuncaoReferencia()
+//    testaTipoFuncaoClasse()
+
+    val minhaFuncaoLambda: () -> Unit = {
+        println("Executa como lambda")
+    }
+    println(minhaFuncaoLambda())
+
+    val minhaFuncaoAnonima: () -> Unit = fun() {
+        println("Executa como anônima")
+    }
+    println(minhaFuncaoAnonima())
+}
+
+private fun testaTipoFuncaoClasse() {
+    val minhaFuncaoClasse: () -> Unit = Teste()
+    println(minhaFuncaoClasse())
+}
+
+fun testaTipoFuncaoReferencia() {
+    var minhaFuncao: () -> Unit = ::teste
+    println(minhaFuncao())
+}
+
+fun teste(){
+    println("executa teste")
+}
+
+class Teste : () -> Unit {
+    override fun invoke() {
+        println("executa invoke do Teste")
+    }
+
 }
